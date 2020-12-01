11ce05c05e2d3f46297a14605e76b8d66e749e21
Jurrien Fakkeldij <jurrien.fakkeldij@gmail.com>
2020-12-01 10:02:41 +0100

    Day1-Complete

---
+++ day1.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"aoc-2020/internal/filereader"
+	"aoc-2020/internal/transformer"
+	"fmt"
+)
+
+func main() {
+	Day1()
+}
+
+func Day1() {
+	fmt.Println("============== Day1 ==============")
+	Part1()
+	Part2()
+}
+
+func Part1() {
+	fmt.Println("============== Part1 =============")
+	testExpenseReport, _ := transformer.SliceAtoi(filereader.ReadFile("./input/day-1/test.txt"))
+	solution := fixExpenseReport(testExpenseReport)
+
+	if solution != 514579 {
+		fmt.Println("Test went wrong:", solution)
+	} else {
+		fmt.Println("Test passed:", solution)
+	}
+
+	expenseReport, _ := transformer.SliceAtoi(filereader.ReadFile("./input/day-1/input.txt"))
+	solution = fixExpenseReport(expenseReport)
+	fmt.Println("Solution:", solution)
+}
+
+func fixExpenseReport(expenses []int) int {
+	correctExpense := 0
+	for _, expense := range expenses {
+		for _, anotherExpense := range expenses {
+			if expense+anotherExpense == 2020 {
+				correctExpense = expense * anotherExpense
+				break
+			}
+		}
+	}
+	return correctExpense
+}
+
+func Part2() {
+	fmt.Println("============== Part2 =============")
+	testExpenseReport, _ := transformer.SliceAtoi(filereader.ReadFile("./input/day-1/test.txt"))
+	solution := fixExpenseReportPart2(testExpenseReport)
+
+	if solution != 241861950 {
+		fmt.Println("Test went wrong:", solution)
+	} else {
+		fmt.Println("Test passed:", solution)
+	}
+
+	expenseReport, _ := transformer.SliceAtoi(filereader.ReadFile("./input/day-1/input.txt"))
+	solution = fixExpenseReportPart2(expenseReport)
+	fmt.Println("Solution:", solution)
+}
+
+func fixExpenseReportPart2(expenses []int) int {
+	correctExpense := 0
+	for _, expense := range expenses {
+		for _, anotherExpense := range expenses {
+			for _, thirdExpense := range expenses {
+				if expense+anotherExpense+thirdExpense == 2020 {
+					correctExpense = expense * anotherExpense * thirdExpense
+					break
+				}
+			}
+		}
+	}
+	return correctExpense
+}
---
+++ internal/transformer/transformer.go
@@ -0,0 +1,15 @@
+package transformer
+
+import "strconv"
+
+func SliceAtoi(sa []string) ([]int, error) {
+	si := make([]int, 0, len(sa))
+	for _, a := range sa {
+		i, err := strconv.Atoi(a)
+		if err != nil {
+			return si, err
+		}
+		si = append(si, i)
+	}
+	return si, nil
+}
